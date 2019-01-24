fa5ea8f60e999b57133f5f88d6d39fb84389a69e
Angela Rivas <snowcats001@gmail.com>
2019-01-24 12:24:09 -0700

    Added a runner and a main method, also made new import reference to OOP C++ Library <iostream>

---
+++ Runner.cpp
@@ -0,0 +1,16 @@
+//
+//  Runner.cpp
+//  CPPFirst
+//
+//  Created by Rivas, Angela on 1/24/19.
+//  Copyright © 2019 CTEC CSP. All rights reserved.
+//
+
+#include <iostream>
+
+int main()
+{
+    
+    
+    return 0;
+}
