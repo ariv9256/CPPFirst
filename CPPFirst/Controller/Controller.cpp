29c20dbe041e426a5259dba833f83368f38ae8ea
Angela Rivas <snowcats001@gmail.com>
2019-01-24 12:31:56 -0700

    Added Controller class with class header, added built in namespace in Controller HPP class.

---
+++ CPPFirst/Controller/Controller.cpp
@@ -0,0 +1,9 @@
+//
+//  Controller.cpp
+//  CPPFirst
+//
+//  Created by Rivas, Angela on 1/24/19.
+//  Copyright © 2019 CTEC CSP. All rights reserved.
+//
+
+#include "Controller.hpp"
---
+++ CPPFirst/Controller/Controller.hpp
@@ -0,0 +1,15 @@
+//
+//  Controller.hpp
+//  CPPFirst
+//
+//  Created by Rivas, Angela on 1/24/19.
+//  Copyright © 2019 CTEC CSP. All rights reserved.
+//
+
+#ifndef Controller_hpp
+#define Controller_hpp
+
+#include <iostream>
+using namespace std;
+
+#endif /* Controller_hpp */
